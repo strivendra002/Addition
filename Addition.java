df702db5fae105edadbdcbe7a5f876e6944ecf28
strivendra002 <strivendra002@gmail.com>
2022-08-24 10:43:59 +0530

    Trivendra

--- Addition.java
+++ Addition.java
@@ -2,8 +2,8 @@ public class Addition{
      public static void main(String[] args) {
         int a =2;
         int b=5;
-        int c=6;
-        int sum=a+b+c;
+        // int c=6;
+        int sum=a+b;
         Systrm.out.ptintln("sum is equal to"+""+sum);
     }
 }
